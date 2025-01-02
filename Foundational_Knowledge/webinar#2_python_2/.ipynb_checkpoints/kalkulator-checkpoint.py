4cdb5560a435c845f4f3e6dc91edae9593230c0b
Marfin12 <fadilahmarfin@gmail.com>
2025-01-02 19:17:42 +0700

    - init catatan bootcamp ai foundational knowledge

---
+++ Foundational_Knowledge/webinar#2_python_2/.ipynb_checkpoints/kalkulator-checkpoint.py
@@ -0,0 +1,14 @@
+PI = 3.1415
+
+def jumlah(angka1, angka2):
+    return angka1 + angka2
+
+def kurang(angka1, angka2):
+    return angka1 - angka2
+
+def kali(angka1, angka2):
+    return angka1*angka2
+
+def bagi(angka1, angka2):
+    return angka1*angka2
+    
